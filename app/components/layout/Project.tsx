fac95e8471057d0598958a1e0e8bf35a945a3879
rizkirenden <rizkirenden@gmail.com>
2025-05-03 19:32:28 +0800

    portofolio

---
+++ app/components/layout/Project.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Project = () => {
+  return (
+    <div className="flex items-center justify-center text-amber-300">
+      Project
+    </div>
+  );
+};
+
+export default Project;
--- app/pages/Landingpage.tsx
+++ app/pages/Landingpage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Card from "../components/layout/Card";
 import Framework from "../components/layout/Framework";
+import Project from "../components/layout/Project";
 export const Landingpage = () => {
   return (
     <div>
       <Card />
       <Framework />
+      <Project />
     </div>
   );
 };
