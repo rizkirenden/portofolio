7e69d69e1b28ba432642c66609b50b75958861dd
rizkirenden <rizkirenden@gmail.com>
2025-05-03 16:10:06 +0800

    portofolio framework

--- app/components/layout/Framework.tsx
+++ app/components/layout/Framework.tsx
@@ -1,6 +1,24 @@
+// components/Framework.tsx
 import React from "react";
+import Saya from "../elements/Saya";
+import { FrameworkData } from "../data/FrameworkData";
 
 export const Framework = () => {
-  return <div></div>;
+  return (
+    <div className="flex justify-center">
+      <div className="flex items-center gap-6 flex-wrap h-10 m-0 p-5 transform rotate-[-10deg] skew-y-[10deg]">
+        {FrameworkData.map((image, index) => (
+          <Saya
+            key={index}
+            src={image.src}
+            alt={image.alt}
+            height={image.height}
+            width={image.width}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
+
 export default Framework;
--- app/pages/Landingpage.tsx
+++ app/pages/Landingpage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Card from "../components/layout/Card";
+import Framework from "../components/layout/Framework";
 export const Landingpage = () => {
   return (
     <div>
       <Card />
+      <Framework />
     </div>
   );
 };
