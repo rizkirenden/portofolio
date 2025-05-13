10897f8d68bc972e22717f15b5e27b42370ae3e2
rizkirenden <rizkirenden@gmail.com>
2025-05-13 19:43:42 +0800

    atoms

---
+++ app/components/atoms/subtitle.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Subtitle = ({ children }: { children: React.ReactNode }) => {
+  return <p className="mb-4 text-lg font-normal text-white">{children}</p>;
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Title } from "../app/components/atoms/title";
+
 export default function Home() {
   return (
     <>
-      <div>
-        <Title />
-      </div>
+      <div></div>
     </>
   );
 }
