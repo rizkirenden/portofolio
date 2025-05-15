e741551e7425a369731dcb48e9720ffa93f33cae
rizkirenden <rizkirenden@gmail.com>
2025-05-15 21:36:37 +0800

    teknologi

--- app/components/molecules/about/cardteknologi.tsx
+++ app/components/molecules/about/cardteknologi.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import { Card } from "../../atoms/card";
 const cardteknologi = () => {
-  return <div>cardteknologi</div>;
+  return (
+    <div>
+      <Card />
+    </div>
+  );
 };
 
 export default cardteknologi;
